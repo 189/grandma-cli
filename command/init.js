ef3fef8f3672b3eb90c82c6cd3b002ddf39ba5d2
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2017-09-05 14:44:39 +0800

    rename templates

--- command/init.js
+++ command/init.js
@@ -20,7 +20,9 @@ module.exports = (name) => {
 
     // 判断传入的模板是否已经在 package.json 中配置对应的 git 仓库地址
     if (!template) {
-        throw new Error(`没有找到对应${template}的模板，请确认当前模板是否存在`);
+        // throw new Error(`没有找到对应${template}的模板，请确认当前模板是否存在`);
+        console.log(errorTip(`x 没有找到对应${name}的模板，请确认当前模板是否存在`));
+        exit(0);
     }
 
     // 获取模板参考名称
@@ -37,25 +39,24 @@ module.exports = (name) => {
             let absolute = path.join(cwd, file);
             let filename = path.posix.basename(absolute);
             if (stats.isDirectory() && filename === dirname) {
-                console.log(errorTip("x 已经存在同名文件夹: " + dirname));
+                console.error(errorTip("x 已经存在同名文件夹: " + dirname));
                 exit(0);
             }
         })
 
-        const spinner = new ora(`正在生成目录 ${dirname}`).start();
+        console.info(`正在克隆模板 ${dirname}...`);
 
         child.exec(order, (error, stdout, stderr) => {
         	var absolute = path.join(cwd, dirname);
             if (error) {
-                console.log(errorTip(error));
+                console.error(errorTip(error));
                 spinner.fail();
                 exit(0);
             }
             // 目录重命名
             fs.renameSync(repo, dirname);
-            console.log(chalk.green(`√ 目录重命名完成`));
-            console.log(chalk.green(`√ 目录生成完毕`));
-            spinner.succeed("目录生成完毕");
+            console.info(chalk.green(`√ 目录重命名完成`));
+            console.info(chalk.green(`√ 目录生成完毕`));
         	exit(0);
         });
     })
